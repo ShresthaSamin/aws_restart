3ce02a18dc9d9014bc74d191fcf09a93dcdb9daf
ShresthaSamin <saminshrestha@yahoo.com>
2024-02-27 18:49:59 +0000

    Added some files for insulin pythomn

---
+++ analyze-insulin.py
@@ -0,0 +1,14 @@
+insulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+print(len(insulin))
+isinsulinseqclean=insulin[0:24]
+print(isinsulinseqclean)
+print(len(isinsulinseqclean))
+binsulinseqclean=insulin[24:54]
+print(binsulinseqclean)
+print(len(binsulinseqclean))
+cinsulinseqclean=insulin[54:89]
+print(cinsulinseqclean)
+print(len(cinsulinseqclean))
+ainsulinseqclean=insulin[89:110]
+print(ainsulinseqclean)
+print(len(ainsulinseqclean))
